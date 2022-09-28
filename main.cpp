6cda0b866c81e0b959d19a3ee8dd5df70aeda304
josemoct99 <jose.ricaurte99@gmail.com>
2022-09-28 11:00:48 -0500

    Menu ingresar información añadido

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ using namespace std;
 //Declaración de las funciones
 void menuPrincipal();
 void menuIngresarInfo();
+void menuAgregarArtista();
+void menuAgregarCancion();
+void menuAgregarAlbum();
+void menuAgregarPlaylist();
 //void menuModificarInfo();
 //void menuBuscarInfo();
 
@@ -34,7 +38,7 @@ void menuPrincipal(){
         cout << "\t\t\t 0. Salir de la aplicación" << endl << endl;		
 		
 		//Lee la opcion elegida del usuario
-		cout << "\t\t\tElige una opción: ";
+		cout << "\t\t\tElige una opción: "<<endl;
 		cin >> opcion;
 		
 		//Alternativas
@@ -77,36 +81,22 @@ void menuIngresarInfo(){
         cout << "\t\t\t 0. Ir atrás" << endl << endl;
 		
 		//Lee la opcion elegida del usuario
-		cout << "\t\t\tElige una opción: ";
+		cout << "\t\t\tElige una opción: "<<endl;
 		cin >> opcion;
 		
 		//Alternativas
 		switch(opcion){
 			case 1:
-				cout << "\t\t\t 1. Cargar por consola" << endl;
-				cout << "\t\t\t 2. Cargar por archivo de texto" << endl;
-				cout << "\t\t\t 0. Ir atrás" << endl;
-				cin >> opcion;
-				switch(opcion){
-					case 1:
-						cout << "Acá se carga por consola" << endl;
-						break;
-					case 2:
-						cout << "Acá se carga por archivo de texto" << endl;
-						break;
-					case 0:
-						menuIngresarInfo();
-						break;			
-				}
+				menuAgregarArtista();
 				break;
 			case 2:
-				//menuModificarInfo();
+				menuAgregarCancion();
 				break;
 			case 3:
-				//menuBuscarInfo();
+				menuAgregarAlbum();
 				break;
 			case 4:
-				//menuBuscarInfo();
+				menuAgregarPlaylist();
 				break;
 			case 0:
 				repetir = false;
@@ -115,3 +105,143 @@ void menuIngresarInfo(){
 		
 	}while(repetir);
 }
+
+
+//Función para agregarArtista
+void menuAgregarArtista(){
+	//mientras sea verdadero se mantendra el menu en pantalla
+	bool repetir = true;
+	//lee la opcion del usuario (elige un numero)
+	int opcion;
+	//Variable para leer el nuevo artista
+	char nomArtista[50];	
+	do{
+		system("cls");
+		cout << "\t\t\t 1. Cargar artista por consola" << endl;
+		cout << "\t\t\t 2. Cargar artista por archivo de texto" << endl;
+		cout << "\t\t\t 0. Ir atrás" << endl;
+		cin >> opcion;
+		switch(opcion){
+			case 1:
+				cout << "\t\t\t Escribe el nombre de la sala" << endl << "\t\t\t";
+				cin.ignore();  //Le pide a getline que ignore el salto de linea hecho anteriormente (endl)
+				cin.getline(nomArtista,50); //Ya que encuentra directamente un cambio de linea se hace lo de arriba
+				cout << "\t\t\t Se ha agregado el artista "<<nomArtista<<endl;
+				system("pause>nul"); //Hará una pausa y no se mostrará nada más en pantalla
+				break;
+			case 2:
+				cout << "Acá se carga por archivo de texto" << endl;
+				system("pause>nul"); //Hará una pausa y no se mostrará nada más en pantalla
+				break;
+			case 0:
+				repetir = false;
+				break;			
+		}
+		
+	}while(repetir);
+}
+
+//Función para agregarCancion
+void menuAgregarCancion(){
+	//mientras sea verdadero se mantendra el menu en pantalla
+	bool repetir = true;
+	//lee la opcion del usuario (elige un numero)
+	int opcion;
+	//Variable para leer el nuevo artista
+	char nomCancion[50];	
+	do{
+		system("cls");
+		cout << "\t\t\t 1. Cargar canción por consola" << endl;
+		cout << "\t\t\t 2. Cargar canción por archivo de texto" << endl;
+		cout << "\t\t\t 0. Ir atrás" << endl;
+		cin >> opcion;
+		switch(opcion){
+			case 1:
+				cout << "\t\t\t Escribe el nombre de la canción" << endl << "\t\t\t";
+				cin.ignore();  //Le pide a getline que ignore el salto de linea hecho anteriormente (endl)
+				cin.getline(nomCancion,50); //Ya que encuentra directamente un cambio de linea se hace lo de arriba
+				cout << "\t\t\t Se ha agregado el artista "<<nomCancion<<endl;
+				system("pause>nul"); //Hará una pausa y no se mostrará nada más en pantalla
+				break;
+			case 2:
+				cout << "Acá se carga por archivo de texto" << endl;
+				system("pause>nul"); //Hará una pausa y no se mostrará nada más en pantalla
+				break;
+			case 0:
+				repetir = false;
+				break;			
+		}
+		
+	}while(repetir);
+}
+
+//Función para agregarAlbum
+void menuAgregarAlbum(){
+	//mientras sea verdadero se mantendra el menu en pantalla
+	bool repetir = true;
+	//lee la opcion del usuario (elige un numero)
+	int opcion;
+	//Variable para leer el nuevo artista
+	char nomAlbum[50];	
+	do{
+		system("cls");
+		cout << "\t\t\t 1. Cargar Album por consola" << endl;
+		cout << "\t\t\t 2. Cargar Album por archivo de texto" << endl;
+		cout << "\t\t\t 0. Ir atrás" << endl;
+		cin >> opcion;
+		switch(opcion){
+			case 1:
+				cout << "\t\t\t Escribe el nombre de la Album" << endl << "\t\t\t";
+				cin.ignore();  //Le pide a getline que ignore el salto de linea hecho anteriormente (endl)
+				cin.getline(nomAlbum,50); //Ya que encuentra directamente un cambio de linea se hace lo de arriba
+				cout << "\t\t\t Se ha agregado el artista "<<nomAlbum<<endl;
+				system("pause>nul"); //Hará una pausa y no se mostrará nada más en pantalla
+				break;
+			case 2:
+				cout << "Acá se carga por archivo de texto" << endl;
+				system("pause>nul"); //Hará una pausa y no se mostrará nada más en pantalla
+				break;
+			case 0:
+				repetir = false;
+				break;			
+		}
+		
+	}while(repetir);
+}
+
+
+//Función para agregarPlaylist
+void menuAgregarPlaylist(){
+	//mientras sea verdadero se mantendra el menu en pantalla
+	bool repetir = true;
+	//lee la opcion del usuario (elige un numero)
+	int opcion;
+	//Variable para leer el nuevo artista
+	char nomPlaylist[50];	
+	do{
+		system("cls");
+		cout << "\t\t\t 1. Cargar Playlist por consola" << endl;
+		cout << "\t\t\t 2. Cargar Playlist por archivo de texto" << endl;
+		cout << "\t\t\t 0. Ir atrás" << endl;
+		cin >> opcion;
+		switch(opcion){
+			case 1:
+				cout << "\t\t\t Escribe el nombre de la Playlist" << endl << "\t\t\t";
+				cin.ignore();  //Le pide a getline que ignore el salto de linea hecho anteriormente (endl)
+				cin.getline(nomPlaylist,50); //Ya que encuentra directamente un cambio de linea se hace lo de arriba
+				cout << "\t\t\t Se ha agregado el artista "<<nomPlaylist<<endl;
+				system("pause>nul"); //Hará una pausa y no se mostrará nada más en pantalla
+				break;
+			case 2:
+				cout << "Acá se carga por archivo de texto" << endl;
+				system("pause>nul"); //Hará una pausa y no se mostrará nada más en pantalla
+				break;
+			case 0:
+				repetir = false;
+				break;			
+		}
+		
+	}while(repetir);
+}
+
+
